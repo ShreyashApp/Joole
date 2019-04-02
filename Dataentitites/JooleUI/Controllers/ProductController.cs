1182f9ae925d78aab3b9633df256b151f43909fd
Charles Yu <cy4qn@virginia.edu>
2019-04-02 11:56:53 -0400

    Updating dummy data

---
+++ Dataentitites/JooleUI/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Services;
+
+namespace JooleUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Summary()
+        {
+            return View();
+        }
+    }
+}
