69c4057441aafb0839bcc7be82f572ca027dd127
shreyash bose <shreyash.imperial@gmail.com>
2019-04-08 09:05:50 -0400

    Design changes and summary page addition.

--- Dataentitites/JooleUI/Controllers/HomeController.cs
+++ Dataentitites/JooleUI/Controllers/HomeController.cs
@@ -4,53 +4,32 @@ using Services;
 using JooleUI.Models;
 using Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace JooleUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Summa()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            Service serv = new Service();
-
-            string vals = "Your application description page." + serv.Value();
-            ViewBag.Message = vals; 
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-           
-            string vals = "Your application description page." ;
-
-            ViewBag.Message = vals;
-
-            return View();
-        }
-
-        public ActionResult GridView()
+        public JsonResult Summ()
         {
             Service serv = new Service();
             List<Products> va = new List<Products>();
-            //List<JObject> ls = new List<JObject>();
-            for(int i = 1; i < 3; i++) {
+            for (int i = 1; i < 2; i++)
+            {
                 Products val = new Products();
                 var a = serv.value(i);
                 val.Product_Name = a.Product_Name;
                 val.Model = a.Model;
                 val.Series = a.Series;
                 val.Product_Image = a.Product_Image;
-                val.Charecteristics = a.Characteristics;
-                JObject json = JObject.Parse(a.Characteristics);
-                val.Object = json;
-                foreach(var key in json)
-                Response.Write(key.Value);
+                //JObject json = JObject.Parse(a.Characteristics);
+                val.Object = a.Characteristics;
+                //Response.Write(val.Object);
                 var b = serv.manudetails(a.Manufacturer_ID);
                 val.Manufacturer_Name = b.Manufacturer_Name;
                 var c = serv.typedetails(a.ProductTypeID);
@@ -60,8 +39,42 @@ namespace JooleUI.Controllers
                 val.MountingLocation = c.MountingLocation;
                 va.Add(val);
             }
+            var cha = JsonConvert.SerializeObject(va);
+            return Json(cha, JsonRequestBehavior.AllowGet);
 
-            return View(va);
+        }
+
+        public ActionResult GridView()
+        {
+            return View();
+        }
+
+        public JsonResult Black()
+        {
+            Service serv = new Service();
+            List<Products> va = new List<Products>();
+            for (int i = 1; i < 3; i++)
+            {
+                Products val = new Products();
+                var a = serv.value(i);
+                val.Product_Name = a.Product_Name;
+                val.Model = a.Model;
+                val.Series = a.Series;
+                val.Product_Image = a.Product_Image;
+                //JObject json = JObject.Parse(a.Characteristics);
+                val.Object = a.Characteristics;
+                //Response.Write(val.Object);
+                var b = serv.manudetails(a.Manufacturer_ID);
+                val.Manufacturer_Name = b.Manufacturer_Name;
+                var c = serv.typedetails(a.ProductTypeID);
+                val.UseType = c.UseType;
+                val.Application = c.Application;
+                val.ModelYear = c.ModelYear;
+                val.MountingLocation = c.MountingLocation;
+                va.Add(val);
+            }
+            var outs = JsonConvert.SerializeObject(va);
+            return Json(outs, JsonRequestBehavior.AllowGet);
         }
 
     }
